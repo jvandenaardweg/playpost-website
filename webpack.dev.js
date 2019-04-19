a35088603dd32584acff3a470cc17609a27dd7d8
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-04-19 13:40:28 +0200

    Added privacy policy and terms of use pages

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,78 +4,88 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    devtool: 'eval-cheap-module-source-map',
-    entry: './src/index.js',
-    devServer: {
-        port: 8080,
-        contentBase: path.join(__dirname, "dist")
-    },
-    node: {
-        fs: 'empty'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                options: {
-                    presets: ['env']
-                }
-            },
-            {
-                test: /\.(scss|css)$/,
-                use: [
-                    {
-                        // creates style nodes from JS strings
-                        loader: "style-loader",
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        // translates CSS into CommonJS
-                        loader: "css-loader",
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        // compiles Sass to CSS
-                        loader: "sass-loader",
-                        options: {
-                            outputStyle: 'expanded',
-                            sourceMap: true,
-                            sourceMapContents: true
-                        }
-                    }
-                    // Please note we are not running postcss here
-                ]
+  devtool: 'eval-cheap-module-source-map',
+  entry: './src/index.js',
+  devServer: {
+    port: 8080,
+    contentBase: path.join(__dirname, "dist")
+  },
+  node: {
+    fs: 'empty'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+        }
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          {
+            // creates style nodes from JS strings
+            loader: "style-loader",
+            options: {
+              sourceMap: true
             }
-            ,
-            {
-                // Load all images as base64 encoding if they are smaller than 8192 bytes
-                test: /\.(png|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            // On development we want to see where the file is coming from, hence we preserve the [path]
-                            name: '[path][name].[ext]?hash=[hash:20]',
-                            limit: 8192
-                        }
-                    }
-                ]
+          },
+          {
+            // translates CSS into CommonJS
+            loader: "css-loader",
+            options: {
+              sourceMap: true
             }
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-            inject: true
-        }),
-        new CopyPlugin([
-          { from: './src/assets/audio/example.wav', to: './assets/audio/example.wav' }
-        ]),
-    ]
+          },
+          {
+            // compiles Sass to CSS
+            loader: "sass-loader",
+            options: {
+              outputStyle: 'expanded',
+              sourceMap: true,
+              sourceMapContents: true
+            }
+          }
+          // Please note we are not running postcss here
+        ]
+      }
+      ,
+      {
+        // Load all images as base64 encoding if they are smaller than 8192 bytes
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              // On development we want to see where the file is coming from, hence we preserve the [path]
+              name: '[path][name].[ext]?hash=[hash:20]',
+              limit: 8192
+            }
+          }
+        ]
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      inject: true
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'privacy-policy.html',
+      template: './privacy-policy.html',
+      inject: true
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'terms-of-use.html',
+      template: './terms-of-use.html',
+      inject: true
+    }),
+    new CopyPlugin([
+      { from: './src/assets/audio/example.wav', to: './assets/audio/example.wav' }
+    ]),
+  ]
 };
