c8d0db0454ee2dc455f719d5071b8104b03787f2
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-04-23 09:42:36 +0200

    Rename

--- webpack.prod.js
+++ webpack.prod.js
@@ -108,7 +108,7 @@ module.exports = {
       // favicon background color (see https://github.com/haydenbleasel/favicons#usage)
       background: '#fff',
       // favicon app title (see https://github.com/haydenbleasel/favicons#usage)
-      title: 'postplay-website}}',
+      title: 'playpost-website}}',
 
       // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage)
       icons: {
