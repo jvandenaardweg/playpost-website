a44dcae706eff09528975eab1a54f41d7e656f7c
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-07-09 13:52:31 +0200

    Adjust cards

--- src/html.js
+++ src/html.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default class HTML extends React.Component {
   render() {
+    const description =
+      'Turn every article on the web into audio using industry leading high quality voices. So you can listen to it just like Podcasts and Audiobooks.'
+
+    const title = 'Playpost - Instant podcasts of every article'
     return (
       <html lang="en">
         <head>
@@ -11,34 +15,38 @@ export default class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
+          <title>{title}</title>
           {this.props.headComponents}
           <link
-            href="/img/apple-touch-icon.png"
+            href={require('../static/img/apple-touch-icon.png')}
             rel="apple-touch-icon"
             sizes="180x180"
           />
-          <link href="/img/favicon.ico" rel="icon" type="image/x-icon" />
-          <meta
-            name="twitter:card"
-            value="Playpost - Instant podcasts of every article"
+          <link
+            href={require('../static/img/favicon.ico')}
+            rel="icon"
+            type="image/x-icon"
           />
-          <meta
-            name="description"
-            content="Turn every article on the web into audio using industry leading high quality voices. So you can listen to it just like Podcasts and Audiobooks."
-          />
-          <meta
-            property="og:title"
-            content="Playpost - Instant podcasts of every article"
-          />
+          <meta name="twitter:app" value="summary" />
+          <meta name="twitter:site" content="@playpostapp" />
+          <meta name="description" content={description} />
+          <meta name="twitter:app:name:iphone" content="Playpost" />
+          <meta name="twitter:app:id:iphone" content="1460663960" />
+          {/* <meta name="twitter:app:url:iphone" content="playpost://" /> */}
+          <meta name="twitter:app:name:ipad" content="Playpost" />
+          <meta name="twitter:app:id:ipad" content="1460663960" />
+          {/* <meta name="twitter:app:url:ipad" content="playpost://" /> */}
+          {/* <meta name="twitter:app:name:googleplay" content="Cannonball"> */}
+          {/* <meta name="twitter:app:id:googleplay" content="io.fabric.samples.cannonball"> */}
+          {/* <meta name="twitter:app:url:googleplay" content="http://cannonball.fabric.io/poem/5149e249222f9e600a7540ef"> */}
+
+          <meta property="og:title" content={title} />
           <meta property="og:url" content="https://playpost.app/" />
           <meta
             property="og:image"
-            content="https://playpost.app//ogimage.2f7d56f7c8d49bba757b.png"
+            content={require('../static/img/ogimage.png')}
           />
-          <meta
-            property="og:description"
-            content="Turn every article on the web into audio using industry leading high quality voices. So you can listen to it just like Podcasts and Audiobooks."
-          />
+          <meta property="og:description" content={description} />
           <meta property="fb:app_id" content="557260604800637" />
           <meta property="og:type" content="website" />
           <link rel="preconnect" href="https://www.google.com" />
